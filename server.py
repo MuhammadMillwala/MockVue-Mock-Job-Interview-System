42cb6757d604abf0e04e308251a6c8d52e6d1017
AsadMinhas <86686592+AsadMinhas782@users.noreply.github.com>
2023-06-10 22:59:46 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
+from __future__ import print_function  # In python 2.7
+import numpy as np
+import pickle
+import os
+from flask import Flask, redirect, url_for, render_template, request, jsonify, send_from_directory, Response
+import time
+import spacy
 import cv2
-import subprocess
 from deepface import DeepFace
 import pandas as pd
 import json
@@ -7,20 +13,7 @@ import sys
 from werkzeug.utils import secure_filename
 import librosa
 import soundfile as sf
-import speech_recognition as speechrecognizer
 import speech_recognition as sr
-from __future__ import print_function # In python 2.7
-import numpy as np
-import pickle
-import os
-from flask import Flask,redirect,url_for,render_template,request,jsonify
-from flask import Response
-import time
-import os
-# !python -m spacy download en_core_web_md
-import spacy
-nlp = spacy.load("en_core_web_md")
-import math
 
 app = Flask(__name__)
 df = pd.read_csv('data/normalized_dataset.csv')
@@ -35,8 +28,7 @@ UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'ogg', 'wav', 'mp3'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-camera = cv2.VideoCapture
+camera = cv2.VideoCapture(0)
 emotion_counts = {
     'angry': 0,
     'disgust': 0,
@@ -49,7 +41,7 @@ emotion_counts = {
 }
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def sentences_similarity(sentence1,sentence2):
+def sentences_similarity(sentence1, sentence2):
     doc1 = nlp(sentence1)
     doc2 = nlp(sentence2)
     similarity = doc1.similarity(doc2)
@@ -57,21 +49,13 @@ def sentences_similarity(sentence1,sentence2):
 
 @app.route('/')
 def index():
-    global similarities
-    global Questions_Arr
-    global Correct_Answer_Arr
-    global User_Answers
-    global All_Text_Details
+    global similarities, Questions_Arr, Correct_Answer_Arr, User_Answers, All_Text_Details, emotion_counts, All_Video_Details
 
     similarities = []
     Questions_Arr = []
     Correct_Answer_Arr = []
     User_Answers = []
     All_Text_Details = []
-
-    global emotion_counts
-    global All_Video_Details
-
 
     All_Video_Details = []
     emotion_counts = {
@@ -83,39 +67,13 @@ def index():
         'surprise': 0,
         'neutral': 0,
         'no_face': 0,
-    } 
+    }
     return render_template('Main_page.html')
 
 @app.route('/home')
 def home():
-    global similarities
-    global Questions_Arr
-    global Correct_Answer_Arr
-    global User_Answers
-    global All_Text_Details
-
-    similarities = []
-    Questions_Arr = []
-    Correct_Answer_Arr = []
-    User_Answers = []
-    All_Text_Details = []
-
-    global emotion_counts
-    global All_Video_Details
-
-
-    All_Video_Details = []
-    emotion_counts = {
-        'angry': 0,
-        'disgust': 0,
-        'fear': 0,
-        'happy': 0,
-        'sad': 0,
-        'surprise': 0,
-        'neutral': 0,
-        'no_face': 0,
-    } 
     return render_template('Main_page.html')
+
 @app.route('/about')
 def about():
     return render_template('Main_page.html')
@@ -143,178 +101,84 @@ def Video_Test():
 @app.route('/Video_Test_Results')
 def Video_Test_Results():
     return render_template('Video_Test_Results.html')
+
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/Questions')
 
 @app.route('/Questions')
 def Text_Questions():
-    global Questions_Arr
-    global Correct_Answer_Arr
+    global Questions_Arr, Correct_Answer_Arr
     random_rows = df.sample(n=10)
     Questions_Arr = random_rows['Questions'].tolist()
     Correct_Answer_Arr = random_rows['Answers'].tolist()
-    return Questions_Arr
+    return jsonify(Questions_Arr)
 
-@app.route('/Text_Answers/<int:Qindex>', methods=['POST'])
-def text_answers(Qindex):
-    global All_Text_Details
-    global Questions_Arr
-    global Correct_Answer_Arr
-
-    answer = request.data.decode('utf-8')
-    temp_list = []
-    temp_list.append(Questions_Arr[Qindex])
-    temp_list.append(Correct_Answer_Arr[Qindex])
-    temp_list.append(answer)
-    temp_list.append(sentences_similarity(str(answer),str(Correct_Answer_Arr[Qindex])))
-    All_Text_Details.append(temp_list)
-    return jsonify(answer)
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/upload-audio/<int:Qindex>', methods=['POST'])
-def upload_audio(Qindex):
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))        
-        audio, speechrecognizer = librosa.load("uploads/audio"+str(Qindex)+".mp3")
-        sf.write("uploads/audio"+str(Qindex)+".wav", audio, speechrecognizer)
-        # open the audio file using the recognizer  
-        r = sr.Recognizer()
-        with sr.AudioFile("uploads/audio"+str(Qindex)+".wav") as source:
-            # listen to the file and store the audio data in a variable
-            audio_data = r.record(source)
-        # use the recognizer to convert speech to text
-        text = r.recognize_google(audio_data)
-        os.remove("uploads/audio"+str(Qindex)+".mp3")
-        os.remove("uploads/audio"+str(Qindex)+".wav")
-        global Questions_Arr
-        global Correct_Answer_Arr
-        global All_Video_Details
-        Similarity = sentences_similarity(str(text), str(Correct_Answer_Arr[Qindex]))
-        temp_list = []
-        temp_list.append(Questions_Arr[Qindex])
-        temp_list.append(Correct_Answer_Arr[Qindex])
-        temp_list.append(text)
-        temp_list.append(Similarity)
-        All_Video_Details.append(temp_list)
-        return jsonify({'success': text}), 200
+@app.route('/Text_Answers/<int:QuestionIndex>', methods=['POST'])
+def Text_Answers(QuestionIndex):
+    global User_Answers, Correct_Answer_Arr, Questions_Arr
+    User_Answers.append(request.form['userAnswer'])
+    if QuestionIndex == 9:
+        score = 0
+        for i in range(10):
+            similarity = sentences_similarity(User_Answers[i], Correct_Answer_Arr[i])
+            similarities.append(similarity)
+            if similarity >= 0.5:
+                score += 1
+        return jsonify(score)
     else:
-        return jsonify({'error': 'Invalid file type'}), 400
+        return jsonify('success')
 
+@app.route('/Video_Answers', methods=['POST'])
+def Video_Answers():
+    global All_Video_Details, emotion_counts
+    video_file = request.files['videoFile']
+    video_filename = secure_filename(video_file.filename)
+    video_file.save(os.path.join(app.config['UPLOAD_FOLDER'], video_filename))
 
-@app.route('/video_results')
-def video_results():
-    temp_Emotion_Counts = emotion_counts
-    temp_Results = All_Video_Details
+    try:
+        cap = cv2.VideoCapture(os.path.join(app.config['UPLOAD_FOLDER'], video_filename))
+        if not cap.isOpened():
+            return jsonify('error')
 
-    temp_Emotion_Counts['angry'] = emotion_counts['angry'] * 0.2
-    temp_Emotion_Counts['disgust'] = emotion_counts['disgust'] * 0.2
-    temp_Emotion_Counts['fear'] = emotion_counts['fear'] * 0.2
-    temp_Emotion_Counts['happy'] = emotion_counts['happy'] * 1.3
-    temp_Emotion_Counts['sad'] = emotion_counts['sad'] * 0.2
-    temp_Emotion_Counts['neutral'] = emotion_counts['neutral'] * 1
-    temp_Emotion_Counts['no_face'] = 0
+        frames_processed = 0
+        face_detected = False
+        emotion_detected = False
 
-    new_temp = [] 
-    new_temp.append(temp_Results)
-    new_temp.append(list(temp_Emotion_Counts.values()))
-    return jsonify(new_temp)
-
-
-@app.route('/text_results')
-def text_results():
-    # All_Text_Details_temp = []
-    All_Text_Details_temp = All_Text_Details    
-    return jsonify(All_Text_Details_temp)
-
-def generate_frames():
-    global camera
-    while True:
-        if camera is not None:
-            success, frame = camera.read()
-            if not success:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
                 break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+            if len(faces) > 0:
+                face_detected = True
+
+                for (x, y, w, h) in faces:
+                    face = frame[y:y + h, x:x + w]
+                    emotion = DeepFace.analyze(face, actions=['emotion'])
+                    emotion_counts[emotion['dominant_emotion']] += 1
+                    emotion_detected = True
+
+            frames_processed += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        if frames_processed > 0 and face_detected and emotion_detected:
+            emotion_ratios = {emotion: count / frames_processed for emotion, count in emotion_counts.items()}
+            All_Video_Details.append(emotion_ratios)
+            return jsonify('success')
         else:
-            break
-            
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame_copy = frame
-        frame = buffer.tobytes()
- 
-        # convert to grayscale
-        gray = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
+            return jsonify('error')
+    except Exception as e:
+        print(str(e))
+        return jsonify('error')
 
-        # detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.3,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+if __name__ == '__main__':
+    app.run(debug=True)
 
-        # loop over faces
-        for (x, y, w, h) in faces:
-            # extract face
-            face = frame_copy[y:y+h, x:x+w]
-            # recognize emotion if a face is detected
-            if len(face) > 0:
-                try:
-                    result = DeepFace.analyze(face, actions=['emotion'], enforce_detection=True)
-                    if result[0]['dominant_emotion'] is not None:
-                        emotion_counts[result[0]['dominant_emotion']] += 1
-                    else:
-                        no_count['no_emotion'] += 1
-                except ValueError as err:
-                    emotion_counts['no_face'] += 1
-            # update the no_face count if no face is detected
-            else:
-                emotion_counts['no_face'] += 1
-
-        yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/videofeed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/start')
-def start():
-    global camera
-    if camera is None:
-        camera = cv2.VideoCapture(0)
-    return 'Camera started'
-
-@app.route('/start_again')
-def start_again():
-    global camera
-    if camera is None:
-        camera = cv2.VideoCapture(0)
-    while not camera.isOpened():  # Wait until the camera is ready
-        pass
-    return 'Camera started'
-
-@app.route('/stop')
-def stop():
-    global camera
-    if camera is not None:
-        camera.release()
-        camera = None
-        camera = cv2.VideoCapture(0)
-    return 'Camera stopped'
-
-if __name__=='__main__':
-    app.run(debug=True,port=8000)
